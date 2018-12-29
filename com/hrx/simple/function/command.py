47694923d7332248a99ac5bbdb671e7f2bbf5d68
Hou,Ruixiang <houruixiang@baidu.com>
2018-12-29 15:33:00 +0800

    函数的参数 位置参数(必选参数) 默认参数 可变参数 关键字参数 命名关键字参数 --update

--- com/hrx/simple/function/command.py
+++ com/hrx/simple/function/command.py
@@ -2,26 +2,98 @@
 import math
 
 
-class common:
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
 
-    def my_abs(x):
-        if x >= 0:
-            return x
-        else:
-            return -x
 
-    def my_abs_check(x):
-        # 类型检查
-        if not isinstance(x, (int, float)):
-            raise TypeError('哈哈哈')
-        if x >= 0:
-            return x
-        else:
-            return -x
+def my_abs_check(x):
+    # 类型检查
+    if not isinstance(x, (int, float)):
+        raise TypeError('哈哈哈')
+    if x >= 0:
+        return x
+    else:
+        return -x
 
-    # 返回多个值
-    # math 机型
-    def move(x, y, step, angle=0):
-        nx = x + step * math.cos(angle)
-        ny = y - step * math.sin(angle)
-        return nx, ny
+
+# 返回多个值
+# math 机型  angle是默认参数
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+
+
+# todo 函数定义默认参数、可变参数和关键字参数
+
+# 位置参数  -- 就是普通的参数
+# 计算 x² 函数1
+def power_1(x):
+    return x * x
+
+
+# 计算 x三次方 函数2
+def power_2(x, n):
+    i = 1
+    while n > 0:
+        n = n - 1
+        i = i * x
+    return i
+
+
+# todo 默认参数  -- 降低函数调用的难度   对于没有特殊要求的使用默认的参数 有需求自己传入相关参数
+# 如兼容函数1 函数2  即同时支持  1个参数和两个参数的调用
+
+def power_common(x, n=2):
+    i = 1
+    while n > 0:
+        n = n - 1
+        i = i * x
+    return i
+
+
+def print_person_info(name, age, city='北京'):
+    print('姓名: %s' % name)
+    print('年龄: %s' % age)
+    print('所在城市: %s' % city)
+
+
+def print_error(tag=[]):
+    tag.append('end')
+    print(tag)
+
+
+def print_curr(tag=None):
+    print(tag)
+
+
+# todo 可变参数  -- 降低函数调用的难度   对于没有特殊要求的使用默认的参数 有需求自己传入相关参数
+def calc(numbers):
+    sum = 0
+    for item in numbers:
+        sum = sum + item * item
+    return sum
+
+
+def calc_command(*numbers):
+    sum = 0
+    for item in numbers:
+        sum = sum + item * item
+    return sum
+
+
+# 这里的other就是 关键字参数   其实也可以传入 tuple或者list
+def print_person_info2(name, age, **kwargs):
+    print('name: ', name, 'age: ', age, 'other: ', kwargs)
+
+
+# 命名关键字参数--关键字参数 key被限制为 city job
+def person(name, age, *, city, job):
+    print('name: ', name, 'age: ', age, 'other: ', city, job)
+
+
+def person_defult(name, age, *args, city='shanghai', job='cooker'):
+    print('name: ', name, 'age: ', age, 'other: ', city, job)
--- com/hrx/simple/test5.py
+++ com/hrx/simple/test5.py
@@ -7,7 +7,10 @@
 # @Software: PyCharm
 
 # 引用
+from typing import Dict
+
 from com.hrx.simple.function import command
+
 
 class test5:
 
@@ -49,44 +52,70 @@ def nop():
 # print(command.my_abs_check('哈哈'))
 
 # 返回多个值的函数
-
 print(command.move(100, 1000, 1))
 
 print('-------------------------------函数的参数------------------------------------------')
 
+# python的函数定义非常简单,但是灵活度却非常大.除了正常的必选参数外,还可以使用
+# todo 默认参数
+# todo 可变参数
+# todo 关键字参数
+# 是的函数定义出来的接口,不但能处理复杂的参数,还可以建华调用者的代码
 
 
+print('-------------------------------位置参数------------------------------------------')
+# 调用普通function  1参  and  两参
+print(command.power_1(10))
+print(command.power_2(10, 3))
+
+print(command.power_common(10))
+print(command.power_common(10, 3))
+
+print('-------------------------------默认参数------------------------------------------')
+# 但是需要注意  默认参数 必须是不可变对象 这样会导致默认值随着函数的调用发生改变
+command.print_error()
+command.print_error()
+command.print_error(['hallo'])
+
+command.print_curr(['end'])
+command.print_curr()
+
+print('-------------------------------可变参数------------------------------------------')
+#  可变参数 顾名思义 就是传入参数的个数是可变的
+#  计算 a2 + b2 + c2 + ……
+#  但是 调用时候要事先组装一个list 或者tuple  作为参数
+print(command.calc([1, 2, 3]))
+
+# 简化一下 在python中 在一个参数 num前面加* 即*num可以把num转化为一个简单的多参数(用,隔开  类似于java中的value...),而这个num就是tuple或者list
+#  这样来说 nums = [1, 2, 3] 那么*num = 1,2,3 这样更加符合一个函数的传参
+print(command.calc_command(1, 2, 3))
+
+print('-------------------------------可变参数 + *------------------------------------------')
+# 也就是说 一个list 或者tuple  可以直接加* 号传入
+nums = [1, 2, 3]
+print(command.calc_command(nums[0], nums[1], nums[2]))
+print(command.calc_command(*nums))
+
+print('-------------------------------关键参数------------------------------------------')
+# 关键参数  其实就是对一个函数的扩展
+# 其中**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，
+# 注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
+# 比如 设定 一个person属性的输出函数  首先 name  age 是必传参数,不同的person有不同的特性 比如 小A会飞 小B会叫 这时候 就需要定义一个关键字参数
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+hallo = {'语文': '90', '数学': '88'}
+command.print_person_info2('张三', 18, city='北京')
+command.print_person_info2('张三', 18, **extra)
+command.print_person_info2('张三', 18, **hallo)
+# 注意这样 print(**extra)是没有结果的
+# **extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数
+# ，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
+
+print('-------------------------------命名关键参数------------------------------------------')
+command.person('jhon', 56, city='beijing', job='docter')
+command.person_defult('bob', 20)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 参数组合
+# 在Python中定义函数，
+# 可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，
+# 参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
