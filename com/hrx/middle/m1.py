e490aedb2488b0305b06bc84e1a3c9da02a7520c
Hou,Ruixiang <houruixiang@baidu.com>
2018-12-29 17:33:52 +0800

    update

---
+++ com/hrx/middle/m1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/29 下午4:59
+# @Author  : Aries
+# @Site    : 
+# @File    : m1.py
+# @Software: PyCharm
+
+
+class m1:
+    # 相当于空构造
+    def __init__(self):
+        pass
+
+# todo python python高级特性 --- 切片
