e5db1b42e8f697927b78d71e37df42ab148bf0d2
houruixiang <18500103402@163.com>
2019-01-27 14:50:33 +0800

    Merge branch 'master' of /Users/houruixiang/python/python-master with conflicts.

--- com/hrx/ood/Screen.py
+++ com/hrx/ood/Screen.py
@@ -13,6 +13,7 @@ class Screen:
         pass
 
     # 因为此时的width方法已经变成了属性，调用的时候直接self.width，所以内部的数据要用self._width更合适，否则会重名造成嵌套死循环
+    # 试想 你把一个function通过@property转化为一个属性with  在func中self.with进行赋值这就造成了死循环
     # 循环次数过多的时候就会造成错误：
     # RecursionError: maximum recursion depth exceeded
     @property
